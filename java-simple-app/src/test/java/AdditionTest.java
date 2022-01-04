4c2c5832881499de2d842109015352e12ec47775
padma.mp <padma.mp@collabera.com>
2022-01-04 12:44:12 +0530

    first commit 2nd

--- java-simple-app/src/test/java/AdditionTest.java
+++ java-simple-app/src/test/java/AdditionTest.java
@@ -2,9 +2,6 @@ public class AdditionTest
 {
     public void testNormalCase(int num1,int num2)
     {
-        int age = 10;
-        float sal = 20.6f;
-        double annualSal = 56.6;
-        char ch = 'a';
+        //return
     }
 }

49708f825ca2d63788cd431fabb26537141f524e
padma.mp <padma.mp@collabera.com>
2022-01-05 13:13:50 +0530

    generic Tests

---
+++ day3/project2/src/main/java/exception/NumDivider.java
@@ -0,0 +1,19 @@
+package exception;
+
+public class NumDivider
+{
+    public double divideNumbers(double num1,double num2)
+    {
+        if(num2 == 0)
+            return 0;
+        return num1/num2;
+    }
+    public  double divideNumberException(double num1,double num2)
+    {
+        if(num2 == 0) throw new ArithmeticException("num2 is 0");
+        return num1/num2;
+    }
+
+    public void divideNumbersException(double num1, int num2) {
+    }
+}

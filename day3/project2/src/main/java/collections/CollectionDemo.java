5ef01ea67ce2d6af45f51dee40c47895004246c1
padma.mp <padma.mp@collabera.com>
2022-01-05 16:18:42 +0530

    java collections: set,list,map,queue operations

--- day3/project2/src/main/java/collections/CollectionDemo.java
+++ day3/project2/src/main/java/collections/CollectionDemo.java
@@ -14,7 +14,14 @@ public class CollectionDemo
         names.add(null);
         names.add("xyz");
 
-        Consumer<String> cls = new Consumer<String>() {
+        names.add(4,"hii");
+        names.contains("abc");
+        names.remove(2);
+        names.remove("xyz");
+
+
+        Consumer<String> cls =new Consumer<String>()
+        {
             @Override
             public void accept(String s) {
                 System.out.println("Name is " + s);
@@ -38,6 +45,8 @@ public class CollectionDemo
         set.add(null);
         set.add("xyz");
 
+        set.remove("xyz");
+
         set.forEach((s) -> System.out.println(s));
 
         return set;
@@ -52,14 +61,53 @@ public class CollectionDemo
         queue.add(null);
         queue.add("pqr");
 
+        queue.offer("android");
+        queue.peek();
+        queue.poll();
+
         queue.forEach(System.out::println);
         return queue;
+
+    }
+
+    public void mapDemo()
+    {
+        Map<Integer,String> map=new HashMap<Integer,String>();
+        map.put(1,"abc");
+        map.put(2,"pqr");
+        map.put(3,"lmn");
+        map.put(4,"xyz");
+
+        Set<Integer> keys = map.keySet();
+        keys.forEach(key -> System.out.println("Key - " +key));
+
+        Collection<String> values = map.values();
+        values.forEach( val -> System.out.println("Value - "+val) );
+
+        map.remove(1, "abc");
+        System.out.println("2 available -> "+map.containsKey(2));;
+        System.out.println("lmn available -> "+map.containsValue("lmn"));;
+        System.out.println("Element At 3 -> "+ map.getOrDefault(3, "none"));
+
+        map.forEach( (k, v) -> System.out.println("Key - "+k +" Value - "+v) );
 
     }
     public  static  void main(System[] args)
     {
         CollectionDemo cmd=new CollectionDemo();
+        System.out.println("List demo");
+        cmd.listDemo();
+        //System.out.println("Set demo");
+        cmd.setDemo();
+        System.out.println("Set demo");
         cmd.queueDemo();
+        System.out.println("Queue demo");
+        cmd.mapDemo();
+        System.out.println("Map demo");
+
+
+
+
     }
 
 }

a986a07898828a63360d86159cf5d65ce1274659
padma.mp <padma.mp@collabera.com>
2022-01-06 15:18:50 +0530

    transaction management

--- day3/jdbc-new/src/main/java/Main.java
+++ day3/jdbc-new/src/main/java/Main.java
@@ -1,33 +1,24 @@
 import db.Connectivity;
 import service.DbService;
-
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Main {
-
     public static void main(String[] args) {
 
         try {
             Connectivity connectivity = new Connectivity();
 
             DbService service = new DbService(connectivity.getConnection());
-//            int cnt = service.create(
-//                    7,
-//                    "klm",
-//                    Date.valueOf(LocalDate.now()),
-//                    true
-//            );
-//            if(cnt > 0) {
-//                System.out.println("Employee Created Successfully");
-//            }
+            int cnt = service.create(10,"klm",Date.valueOf(LocalDate.now()),true);
 
-//            service.find();
-
-//            service.findEmployeeByName("abc");
-
-            service.txnDemo(60, "kol", Date.valueOf(LocalDate.now()), true, 100);
+            if(cnt > 0) {
+                System.out.println("Employee Created Successfully");
+            }
+          //  service.find();
+            //service.findEmployeeByName("abc");
+//            service.txnDemo(60, "kol", Date.valueOf(LocalDate.now()),true, 100);
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
--- day3/jdbc-new/src/main/java/db/Connectivity.java
+++ day3/jdbc-new/src/main/java/db/Connectivity.java
@@ -15,6 +15,7 @@ public class Connectivity {
                 "root",
                 "password"
         );
+        connection.setAutoCommit(false);
 
         System.out.println("---- Connected to MySQL Successfully ----");
     }

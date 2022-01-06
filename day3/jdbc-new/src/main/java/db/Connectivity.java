3acb8de8a645ae0e77f5e469b64ee4aa3fc172ce
padma.mp <padma.mp@collabera.com>
2022-01-06 12:22:59 +0530

    jdbc:First connection

---
+++ day3/jdbc-new/src/main/java/db/Connectivity.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connectivity {
+    private final Connection connection;
+
+    public Connectivity() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/java_traning_db",
+                "root",
+                "password"
+        );
+
+        System.out.println("---- Connected to MySQL Successfully ----");
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}

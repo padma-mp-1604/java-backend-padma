02e23f2c5b51979d43eb85f8c284c66cd86d3412
padma.mp <padma.mp@collabera.com>
2022-01-07 16:35:19 +0530

    spring basic

---
+++ day5/spring-basic-app/src/main/java/car/Car.java
@@ -0,0 +1,12 @@
+package car;
+
+public class Car {
+    private String model;
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+}

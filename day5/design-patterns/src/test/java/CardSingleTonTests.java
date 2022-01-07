3b2fedb678f03842d4bb189278f101c85c16be1d
padma.mp <padma.mp@collabera.com>
2022-01-07 14:07:58 +0530

    design patterns : factory design

---
+++ day5/design-patterns/src/test/java/CardSingleTonTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import singleton.CarSingleton;
+public class CardSingleTonTests {
+    @DisplayName("Check Car Instance is Singleton")
+    @Test
+    void testCarSingleton() {
+        var c1 =  CarSingleton.newInstance();
+        var c2 = CarSingleton.newInstance();
+
+        Assertions.assertEquals(
+                c1.hashCode(),
+                c2.hashCode()
+        );
+    }
+}

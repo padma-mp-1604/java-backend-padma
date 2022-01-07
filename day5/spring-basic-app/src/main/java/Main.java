eb304a719150260fb1d7b8fe31059c71457add54
padma.mp <padma.mp@collabera.com>
2022-01-07 17:55:04 +0530

    spring ioc container and bean

--- day5/spring-basic-app/src/main/java/Main.java
+++ day5/spring-basic-app/src/main/java/Main.java
@@ -1,13 +1,22 @@
 import car.Car;
-import car.Driver;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Car car = new Car();
-        Driver driver = new Driver();
-        //driver.setCar(car);
-        driver.setCar(car);
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Car car1 = (Car) context.getBean("car");
+        car1.setSpeed(100);
+
+        Class<Car> cls = Car.class; // this is not the bytecode class file
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is "+ car2.getSpeed());
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
-
---
+++ day5/spring-basic-app/src/main/java/tempo/Tempo.java
@@ -0,0 +1,14 @@
+package tempo;
+import driver.Driver;
+
+public class Tempo {
+    private Driver driver;
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}

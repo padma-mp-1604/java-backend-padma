6c3c7a4a2dd4447d08a5a14fb70bba5caa25ca28
padma.mp <padma.mp@collabera.com>
2022-01-10 14:58:30 +0530

    spring core: component scanning and aop before

--- week-2/day-1/spring-app-annotationbased/src/main/java/com/ani/car/logging/CarLogger.java
+++ week-2/day-1/spring-app-annotationbased/src/main/java/com/ani/car/logging/CarLogger.java
@@ -4,16 +4,25 @@ package com.ani.car.logging;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CarLogger {
+    @Pointcut("execution(public * com.ani.car.Car.speedUp(..))")
+public void pointCutAfterExecution() { }
+
     @Before("execution(* com.ani.car.*.*(..))")
     public void logBeforeAnyMethodExecutionCarPackage(JoinPoint jp) {
         System.out.println("Before Execution I am getting printed");
         System.out.println("Executing method "+jp.getSignature());
         System.out.println("After this line you will see method output");
         System.out.println("----------------");
+    }@After("pointCutAfterExecution()")
+    public void logAfterAnyMethods(JoinPoint jp) {
+        System.out.println("After Execution I am getting printed");
     }
+
 }

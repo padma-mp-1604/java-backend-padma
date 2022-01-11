45056eaf013439d5bb6567b21f5dad9d8b66e226
padma.mp <padma.mp@collabera.com>
2022-01-11 15:55:30 +0530

    spring mvc: add operation

---
+++ week-2/day-2nd/mvc/mvc/src/main/java/com/padma/mvc/controller/CalcController.java
@@ -0,0 +1,26 @@
+package com.padma.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/calc")
+public class CalcController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("postData", new PostData());
+        return "home";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute PostData postData, BindingResult err, Model model) {
+        model.addAttribute("result", postData);
+        return "result";
+    }
+}

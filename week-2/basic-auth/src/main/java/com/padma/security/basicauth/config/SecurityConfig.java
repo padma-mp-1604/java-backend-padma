6f93da8709a5ca0d457d8fdc37046aaa36c65e84
padma.mp <padma.mp@collabera.com>
2022-01-13 18:43:03 +0530

    spring security : basic authentication

---
+++ week-2/basic-auth/src/main/java/com/padma/security/basicauth/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.padma.security.basicauth.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()// this is not a browser app, this is rest app . so disable csrf
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void loadUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("padma")
+                .password("{noop}password")
+                .roles("ADMIN");
+    }
+}

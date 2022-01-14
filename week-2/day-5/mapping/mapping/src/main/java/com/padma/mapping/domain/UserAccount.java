44f2a92ecf000c1a6678a12be2ffa1c9607f03a4
padma.mp <padma.mp@collabera.com>
2022-01-14 18:13:23 +0530

    entity mapping

---
+++ week-2/day-5/mapping/mapping/src/main/java/com/padma/mapping/domain/UserAccount.java
@@ -0,0 +1,18 @@
+package com.padma.mapping.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date created;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private AppUser user;
+}
---
+++ week-2/day-5/mapping/mapping/src/test/java/com/padma/mapping/MappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padma.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

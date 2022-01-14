9e48c8e27ad9437d8bdad9eb79842759d1c0c748
padma.mp <padma.mp@collabera.com>
2022-01-14 10:44:05 +0530

    unit testing rest layer

---
+++ week-2/day-5/unit-testing-demo/src/main/java/com/padma/unittestingdemo/UnitTestingDemoApplication.java
@@ -0,0 +1,14 @@
+package com.padma.unittestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UnitTestingDemoApplication.class, args);
+	}
+
+}
---
+++ week-2/day-5/unit-testing-demo/src/test/java/com/padma/unittestingdemo/UnitTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padma.unittestingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

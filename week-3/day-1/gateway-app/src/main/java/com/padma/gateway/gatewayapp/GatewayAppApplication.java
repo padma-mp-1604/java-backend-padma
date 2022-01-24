68c29518438db6a36b48c5ab8231edaa65adf818
padma.mp <padma.mp@collabera.com>
2022-01-24 10:22:25 +0530

    microservices

---
+++ week-3/day-1/gateway-app/src/main/java/com/padma/gateway/gatewayapp/GatewayAppApplication.java
@@ -0,0 +1,13 @@
+package com.padma.gateway.gatewayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAppApplication.class, args);
+	}
+
+}

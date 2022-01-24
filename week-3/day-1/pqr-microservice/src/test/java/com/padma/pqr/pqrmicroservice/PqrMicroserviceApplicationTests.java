09c58f8d70cff515aa95eb8cae63a1896932c8bc
padma.mp <padma.mp@collabera.com>
2022-01-24 10:23:29 +0530

    inter service communication

---
+++ week-3/day-1/pqr-microservice/src/test/java/com/padma/pqr/pqrmicroservice/PqrMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padma.pqr.pqrmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

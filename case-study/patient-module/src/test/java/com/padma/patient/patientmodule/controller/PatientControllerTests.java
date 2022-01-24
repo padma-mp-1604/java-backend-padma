40274904dbeceebaa4f05b74d7f899e77b40239c
padma.mp <padma.mp@collabera.com>
2022-01-24 16:33:39 +0530

    final casestudy: patient module

--- case-study/patient-module/src/test/java/com/padma/patient/patientmodule/controller/PatientControllerTests.java
+++ case-study/patient-module/src/test/java/com/padma/patient/patientmodule/controller/PatientControllerTests.java
@@ -1,6 +1,8 @@
 package com.padma.patient.patientmodule.controller;
 
 import com.padma.patient.patientmodule.domain.Patient;
+import com.padma.patient.patientmodule.dto.AppResponse;
+import com.padma.patient.patientmodule.dto.PatientDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PatientControllerTests {
@@ -17,6 +22,20 @@ public class PatientControllerTests {
     @LocalServerPort
     private int port;
 
+    @DisplayName("POST - Patient - Saving Patient Object")
+    @Test
+    public void testPostMethod() {
+        String url = "http://" + "localhost" + ":" + port + "/patient";
+        var patient = new PatientDto();
+        patient.setId(1);
+        patient.setName("vidya");
+        patient.setMobile("7678938991");
+        patient.setStatus(false);
+        patient.setLastVisited(LocalDate.of(2022, 1, 12));
+        var re = template.postForEntity(url, patient, AppResponse.class);
+        Assertions.assertEquals(HttpStatus.OK, re.getStatusCode());
+    }
+
     @DisplayName("Get Patient Checking Object Is non null")
     @Test
     public void testGetMethod(){
@@ -24,6 +43,7 @@ public class PatientControllerTests {
         Patient patient = template.getForObject(url,Patient.class);
         Assertions.assertNotNull(patient);
     }
+
     @DisplayName("Patient Delete")
     @Test
     public void testDeleteMethod(){
